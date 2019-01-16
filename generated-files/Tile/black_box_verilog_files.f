d20e30fe8269ec967b743d0d24ec2b2fe236f6a8
SunnyChen <groupsun@msn.cn>
2019-01-16 15:38:07 +0800

    remove generated-files

--- generated-files/Tile/black_box_verilog_files.f
+++
@@ -1,3 +0,0 @@
-/home/sunnychen/Documents/riscv-mini-five-stage/Tile.InstCache.cache.v
-/home/sunnychen/Documents/riscv-mini-five-stage/Tile.RegFile.regfile.v
-/home/sunnychen/Documents/riscv-mini-five-stage/Tile.DataCache.cache.v
